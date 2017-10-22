71a2330372ae8d1f6e92124eb2e0be07ebcc7e8a
khaerulroh <32412463+khaerulroh@users.noreply.github.com>
2017-10-22 16:52:44 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,11 +2,11 @@ const LineConnect = require('./connect');
 let LINE = require('./main.js');
 
 const auth = {
-	authToken: 'authToken_kamu',
-	certificate: 'certificate_kamu',
+	authToken: 'Em8GolGz1f2CHTzJjR66.mqy4jOUUhIvI0uMEs6ZW9G.dCl79CQeJu/mL39e4K6EEc/NYzBudjkjkmaQkiOhZqLJc',
+	certificate: 'f543cf3185229a2dd4a7e1f12e7958a87c6c534e9d63c993da51ff4d1440513d',
 }
-// let client =  new LineConnect(auth);
-let client =  new LineConnect();
+let client =  new LineConnect(auth);
+//let client =  new LineConnect();
 
 client.startx().then(async (res) => {
 	
